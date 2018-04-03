b61aac5a3031659df8ac62858e6b7e4e7747cb97
adeeb-stwodah <37350689+adeeb-stwodah@users.noreply.github.com>
2018-04-02 23:11:21 -0400

    Add files via upload

---
+++ enter_value_screen.java
@@ -0,0 +1,11 @@
+package adeeb.ia4;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class enter_value_screen extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.enter_value_screen);
+    }
+}
---
+++ resullt.java
@@ -0,0 +1,14 @@
+package adeeb.ia4;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class resullt extends Activity {
+
+    public class enter_value_screen extends Activity {
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.result);
+        }
+
+}
